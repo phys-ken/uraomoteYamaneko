ca403e4ae82e59d4836a932f1004b78df4118166
phys-ken <cromartie24@icloud.com>
2021-08-16 22:31:56 +0900

    first commit

---
+++ filemaker.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import shutil
+import subprocess
+from PIL import Image, ImageFilter , ImageDraw , ImageFont
+
+
+
+def make_image(screen, bgcolor, filename):
+    """
+    画像の作成
+    """
+    num = os.path.splitext(os.path.basename(filename))[0] 
+    print(num)
+    num = int(num)
+    if num%2 == 1:
+      label = str(int((num+1)/2)) +"_omote"
+    else:
+      label = str(int(num/2)) +"_ura"
+
+    img = Image.new('RGB', screen,bgcolor)
+    draw = ImageDraw.Draw(img)# im上のImageDrawインスタンスを作る
+    font = ImageFont.truetype("Arial.ttf", 32)
+    draw.text((30,30),label , font = font)
+    img.save(filename)
+    return
+
+
+if os.path.isdir("./test"):
+  shutil.rmtree("./test")
+
+os.makedirs("./test", exist_ok=True)
+
+for i  in range(1,101):
+  print(i)
+  make_image((300,300), "red", "./test/" + str(i).zfill(4) + ".jpg")
---
+++ uraomote.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import glob
+import re
+import shutil
+from tkinter import filedialog
+import tkinter
+from functools import partial
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+global img
+global ret
+ret = 0
+global flag
+flag = True
+
+
+def modosu():
+    if ret == 0:
+      if not flag == True:
+        messagebox.showerror("エラー","まずはフォルダを選択してください")
+      return 0
+    oyapath = ret
+    kopaths = []
+    kopaths.append(oyapath + "/omote")
+    kopaths.append(oyapath + "/ura")
+    kopaths.append(oyapath + "/yamaneko")
+
+    for ko in kopaths:
+        files = glob.glob(ko + "/*")
+        for f in files:
+            print(f)
+            shutil.move(f, oyapath)
+
+    for ko in kopaths:
+      files = os.listdir(ko)
+      if files:
+        print(ko + "__の中にファイルがあるので、削除しせんでした。")
+        print(files)
+      else:
+        print(ko + "__は空なので、削除します。")
+        shutil.rmtree(ko)
+      
+
+
+def furiwake():
+    if ret == 0:
+      if not flag == True:
+        messagebox.showerror("エラー","まずはフォルダを選択してください")
+      return 0
+    dirpath = ret
+    print(dirpath)
+    os.makedirs(dirpath + "/omote", exist_ok=True)
+    os.makedirs(dirpath + "/ura", exist_ok=True)
+    os.makedirs(dirpath + "/yamaneko", exist_ok=True)
+
+    if not os.path.isdir(dirpath):
+        print("フォルダが存在しません。作業をやめます。")
+        sys.exit()
+
+    files = os.listdir(dirpath)
+    files_file = [f for f in files if os.path.isfile(os.path.join(dirpath, f))]
+    files_file.sort()
+
+    for f in files_file:
+        f = dirpath + "/" + f
+        print(f)
+        num = re.findall(r"\d+", f)
+        if len(num) == 0:
+            shutil.move(f, dirpath + "/yamaneko")
+        elif int(num[-1]) % 2 == 1:
+            shutil.move(f, dirpath + "/omote")
+        else:
+            shutil.move(f, dirpath + "/ura")
+
+
+
+
+class Application(tkinter.Frame):
+    global ret
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title('ウラオモテヤマネコ')
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        global img
+        img = Image.open('./appfigs/top.png')
+        img = ImageTk.PhotoImage(img)
+ 
+        self.canvas = tkinter.Canvas(bg="white", width=300, height=300)
+        self.canvas.pack(anchor=tkinter.NW)
+        self.canvas.create_image(0, 0, image=img, anchor=tkinter.NW)
+
+        self.dialog_button = tkinter.Button(self, text='画像が入っているフォルダを選択...', command=file_open, width=30,height=3)
+        self.dialog_button.pack(anchor=tkinter.NW)
+
+        self.text_1 = tkinter.StringVar()
+        self.type_label = tkinter.Label(self, textvariable=self.text_1 , height= 4 ,wraplength=300)
+        self.type_label.pack(anchor=tkinter.W)
+        self.text_2 = tkinter.StringVar()
+        self.content_label = tkinter.Label(self, textvariable=self.text_2)
+        self.content_label.pack(anchor=tkinter.W)
+
+        self.furiwake_button = tkinter.Button(self, text='フォルダ内の画像を分ける', command=furiwake, width=30,height=3).pack(anchor=tkinter.NW)
+        self.modosu_button = tkinter.Button(self, text='振り分けた画像を戻す', command=modosu, width=30,height=3).pack(anchor=tkinter.NW)
+        global flag
+        flag = False
+def file_open():
+    global ret
+    global flag
+    ini_dir = './'
+    ret = tkinter.filedialog.askdirectory(initialdir=ini_dir, title='画像の入ったフォルダを選択してください', mustexist = True)
+
+    if not len(ret) == 0:    
+      app.text_1.set("フォルダ名:" + str(ret))
+      app.text_2.set("処理の種類を選択してニャ")
+
+root = tkinter.Tk()
+app = Application(master=root)
+app.mainloop()
+
